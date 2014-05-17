4af5a46f8b08601dd1a15d8623634a42ace62ffc
zsolt.doma <zsolt.doma@gmail.com>
2014-05-17 23:51:56 +0200

    Clean up.

--- timetracker-api/src/main/java/hu/zsdoma/timetracker/api/TimeTracker.java
+++ timetracker-api/src/main/java/hu/zsdoma/timetracker/api/TimeTracker.java
@@ -1,59 +1,60 @@
 package hu.zsdoma.timetracker.api;
 
-import java.util.List;
-
 import hu.zsdoma.timetracker.api.dto.WorklogDTO;
+
+import java.util.List;
 
 /**
  * TimeTracker interface for provide functionality.
  */
 public interface TimeTracker {
 
-	/**
-	 * Persist the given worklog.
-	 * 
-	 * @param worklogDTO
-	 *            Given {@link WorklogDTO} reference.
-	 */
-	void newWorklog(WorklogDTO worklogDTO);
+    /**
+     * List all worklogs.
+     * 
+     * @return List of all stored worklogs.
+     */
+    List<WorklogDTO> list();
 
-	/**
-	 * Update the given worklog.
-	 * 
-	 * @param worklogDTO
-	 *            {@link WorklogDTO} reference.
-	 */
-	void updateWorklog(WorklogDTO worklogDTO);
+    /**
+     * List worklogs by given day in timestamp.
+     * 
+     * @param timestamp
+     *            Timestamp // FIXME javadoc
+     * @return {@link WorklogDTO} reference list.
+     */
+    List<WorklogDTO> listByDay(long timestamp);
 
-	/**
-	 * Remove worklog by given worklogId.
-	 * 
-	 * @param worklogId
-	 *            worklogId.
-	 */
-	void removeWorklog(long worklogId);
+    /**
+     * List worklogs by given issue id.
+     * 
+     * @param issueId
+     *            Issue id.
+     * @return {@link WorklogDTO} reference list.
+     */
+    List<WorklogDTO> listByIssue(long issueId);
 
-	/**
-	 * List all worklogs.
-	 * 
-	 * @return List of all stored worklogs.
-	 */
-	List<WorklogDTO> list();
+    /**
+     * Persist the given worklog.
+     * 
+     * @param worklogDTO
+     *            Given {@link WorklogDTO} reference.
+     */
+    void newWorklog(WorklogDTO worklogDTO);
 
-	/**
-	 * List worklogs by given day in timestamp.
-	 * 
-	 * @param timestamp Timestamp // FIXME javadoc
-	 * @return {@link WorklogDTO} reference list.
-	 */
-	List<WorklogDTO> listByDay(long timestamp);
+    /**
+     * Remove worklog by given worklogId.
+     * 
+     * @param worklogId
+     *            worklogId.
+     */
+    void removeWorklog(long worklogId);
 
-	/**
-	 * List worklogs by given issue id.
-	 * 
-	 * @param issueId
-	 *            Issue id.
-	 * @return {@link WorklogDTO} reference list.
-	 */
-	List<WorklogDTO> listByIssue(long issueId);
+    /**
+     * Update the given worklog.
+     * 
+     * @param worklogDTO
+     *            {@link WorklogDTO} reference.
+     */
+    void updateWorklog(WorklogDTO worklogDTO);
 }
--- timetracker-core/src/main/java/hu/zsdoma/timetracker/DefaultTimeTracker.java
+++ timetracker-core/src/main/java/hu/zsdoma/timetracker/DefaultTimeTracker.java
@@ -1,49 +1,49 @@
 package hu.zsdoma.timetracker;
 
-import java.util.List;
-
 import hu.zsdoma.timetracker.api.TimeTracker;
 import hu.zsdoma.timetracker.api.dto.WorklogDTO;
+
+import java.util.List;
 
 /**
  * Default {@link TimeTracker} implementation.
  */
 public class DefaultTimeTracker implements TimeTracker {
 
-	@Override
-	public void newWorklog(WorklogDTO worklogDTO) {
-		// TODO Auto-generated method stub
+    @Override
+    public List<WorklogDTO> list() {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
-	}
+    @Override
+    public List<WorklogDTO> listByDay(final long timestamp) {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
-	@Override
-	public void updateWorklog(WorklogDTO worklogDTO) {
-		// TODO Auto-generated method stub
+    @Override
+    public List<WorklogDTO> listByIssue(final long issueId) {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
-	}
+    @Override
+    public void newWorklog(final WorklogDTO worklogDTO) {
+        // TODO Auto-generated method stub
 
-	@Override
-	public void removeWorklog(long worklogId) {
-		// TODO Auto-generated method stub
+    }
 
-	}
+    @Override
+    public void removeWorklog(final long worklogId) {
+        // TODO Auto-generated method stub
 
-	@Override
-	public List<WorklogDTO> list() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    }
 
-	@Override
-	public List<WorklogDTO> listByDay(long timestamp) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public void updateWorklog(final WorklogDTO worklogDTO) {
+        // TODO Auto-generated method stub
 
-	@Override
-	public List<WorklogDTO> listByIssue(long issueId) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    }
 
 }
